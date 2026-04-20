3f14b9aa5368a111cc3a678b4ce008392d3d018b
MarkEugine16 <markeugineg@gmail.com>
2026-04-20 23:51:55 +0800

    fix firebase env parsing

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,22 +1,29 @@
+
 'use strict';
 
 const admin = require('firebase-admin');
+const path  = require('path');
 const logger = require('../utils/logger');
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
-);
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
-
-  logger.info('Firebase Admin SDK initialized.');
+let serviceAccount;
+try {
+const keyPath = path.resolve(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+serviceAccount = require(keyPath);
+} catch (err) {
+logger.error('Firebase service account key not found. Check FIREBASE_SERVICE_ACCOUNT_KEY in .env');
+process.exit(1);
 }
 
-const db = admin.firestore();
+if (!admin.apps.length) {
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount)
+});
+logger.info('Firebase Admin SDK initialized.');
+}
 
+const db     = admin.firestore();
+
+// Enable Firestore timestamps in snapshots
 db.settings({ ignoreUndefinedProperties: true });
 
 module.exports = { admin, db };
